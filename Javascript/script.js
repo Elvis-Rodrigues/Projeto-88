a7b6bd7496a3b85c379c0e2d3dfc4256dcd0f74d
Elvis-Rodrigues <72317124+Elvis-Rodrigues@users.noreply.github.com>
2022-09-15 07:12:19 -0300

    add arquivos

---
+++ Javascript/script.js
@@ -0,0 +1,30 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#img img")
+    max = images.length;
+
+function nextImage(){
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start(){
+    setInterval(() => {
+        nextImage()
+    }, time);
+    
+}
+
+window.addEventListener("load", start)
+
+
